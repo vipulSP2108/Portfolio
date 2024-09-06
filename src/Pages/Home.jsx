cbb10d07b374b83356a1c072708bea4c0e79aaf7
Vipul Sunil Patil <vipul.patil@iitgn.ac.in>
2024-09-07 03:04:29 +0530

    Home Cursor

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   const [curr, setCurr] = useState(0);
   const bgRef = useRef(null);
   const divRef = useRef(null);
+  const ReactTextRef = useRef(null);
 
   const cursorOutline = useRef(null);
   const cursorDot = useRef(null);
@@ -66,12 +67,12 @@ export default function Home() {
       const element = bgRef.current;
       const animationDuration = 30; // Random duration between 5s and 15s
       const keyframes = `@keyframes one { ` +
-      `0% { left: 0px; top: 0px; transform: rotate(0deg) scale(0.9); } ` +
-      `25% { left: 0px; top: 0px; transform: rotate(90deg) scale(1); } ` +
-      `50% { left: 0px; top: 0px; transform: rotate(180deg) scale(1.1); } ` +
-      `75% { left: 0px; top: 0px; transform: rotate(270deg) scale(0.8); } ` +
-      `100% { left: 0px; top: 0px; transform: rotate(360deg) scale(0.9); } ` +
-  `}`;
+        `0% { left: -10px; top: 10px; transform: rotate(0deg) scale(1); } ` +
+        `25% { left: 10px; top: 10px; transform: rotate(90deg) scale(0.8); } ` +
+        `50% { left: 10px; top: -10px; transform: rotate(180deg) scale(1.1); } ` +
+        `75% { left: -10px; top: -10px; transform: rotate(270deg) scale(0.9); } ` +
+        `100% { left: -10px; top: 10px; transform: rotate(360deg) scale(1); } ` +
+        `}`;
 
       const styleSheet = document.createElement("style");
       styleSheet.type = "text/css";
@@ -88,11 +89,42 @@ export default function Home() {
     return () => clearInterval(intervalId);
   }, []);
 
+
+  useEffect(() => {
+    const ReactTextAnimation = () => {
+      if (!ReactTextRef.current) return;
+
+      const ReactElement = ReactTextRef.current;
+
+      const animationDuration = 5; // Random duration between 5s and 15s
+      const keyframes = `@keyframes ReactTextAnimation { ` +
+        `0% { background-position: 0% 0%; } ` +
+        `25% { background-position: 25% 25%; } ` +
+        `50% { background-position: 50% 50%; } ` +
+        `75% { background-position: 75% 75%; } ` +
+        `100% { background-position: 100% 100%; } ` +
+        `}`;
+
+      const styleSheet = document.createElement("style");
+      styleSheet.type = "text/css";
+      styleSheet.innerText = keyframes;
+      document.head.appendChild(styleSheet);
+
+      ReactElement.style.animation = `ReactTextAnimation ${animationDuration}s infinite alternate`;
+      // element.style.animation = `breathe 8s linear infinite`;
+    };
+
+    ReactTextAnimation();
+    const intervalId = setInterval(ReactTextAnimation, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className='flex flex-col items-center justify-center w-full h-screen text-center'>
-      <div className='cursor flex items-center justify-center'>
+    <div className='z-0 flex flex-col items-center justify-center w-full h-screen text-center'>
+      {/* <div className='cursor flex items-center justify-center'>
         <div ref={cursorOutline} className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] '>
-          <div className=' z-50 fixed h-14 w-14  rounded-full border-2 border-white top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'></div>
+          <div className=' z-10 fixed h-14 w-14  rounded-full border-2 border-black top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'></div>
           <div
             style={{
               opacity: 0.6,
@@ -102,22 +134,35 @@ export default function Home() {
               height: '400px', borderRadius: '9999px',
               // background: `linear-gradient(90deg, #3effe8 1.98%, #3effe8 1.99%, #8c0fee)`,
               background: `linear-gradient(90deg, rgb(140, 0, 215) 1.98%, rgb(225, 72, 173) 1.99%, rgb(30, 0, 225))`,
-              zIndex: 0,
             }}
             ref={bgRef}
           />
         </div>
-        <div ref={cursorDot} className=' fixed h-4 w-4 bg-white rounded-full top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]' />
-      </div>
+        <div ref={cursorDot} className=' z-50 fixed h-4 w-4 bg-black rounded-full top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]' />
+      </div> */}
 
-      <div>
+      <div className=' z-30'>
         <div
-          style={{ zIndex: 1, }}
+          // style={{ zIndex: 1, }}
           className=' w-screen  text-7xl font-black'>
-          <div style={{ zIndex: 1, }}>Together, Let’s</div>
+          <div>Together, Let’s</div>
           <div>
-            <span>React </span>
-            to Your Vision!
+            <span
+              ref={ReactTextRef}
+              style={{
+                // display: 'inline-block', //
+                background: 'linear-gradient(45deg, #65DCF8, #6EB7FF, #005FEA, #504FFB)',
+                // backgroundClip: 'text', //
+                WebkitBackgroundClip: 'text',
+                // color: 'transparent',
+                WebkitTextFillColor: 'transparent',
+                // textFillColor: 'transparent', //
+                backgroundSize: '1000% 1000%',
+                // animation: 'gradient-animation 3s ease infinite',
+              }}
+            >
+              React
+            </span> to Your Vision!
           </div>
         </div>
         <div className=' font-["Poppins"] text-xl font-normal mt-6 text-[#888888]'>
